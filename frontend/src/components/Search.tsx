5bd460ab8c20b8c61d99e39a99af38a5022bea46
Ahmet <a.kntr93@gmail.com>
2022-04-05 21:28:34 +0300

    Add mui & home page

---
+++ frontend/src/components/Search.tsx
@@ -0,0 +1,35 @@
+import '../styles/search.scss';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const cities = ['San Francisco', 'Los Angeles', 'Chicago', 'Boston', 'Seattle'];
+
+export default function Search() {
+  const [val, setVal] = useState<string | null>('');
+  const [inputVal, setInputVal] = useState('');
+
+  return (
+    <div className="search">
+      <h1>Order food to your door</h1>
+      <div className="search-auto-find">
+        <Autocomplete
+          className="auto"
+          value={val}
+          onChange={(event: any, newValue: string | null) => {
+            setVal(newValue);
+          }}
+          inputValue={inputVal}
+          onInputChange={(event, newInputValue) => {
+            setInputVal(newInputValue);
+          }}
+          options={cities}
+          renderInput={(params) => (
+            <TextField variant="filled" {...params} label="Enter delivery address" />
+          )}
+        />
+        <button className="find">Find Food</button>
+      </div>
+    </div>
+  );
+}
